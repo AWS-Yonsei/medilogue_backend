5ebbd2a81b79a2a8b02a4cb1fd258b7d9745d29b
vesselofgod <vesselofgod@naver.com>
2024-01-30 17:00:01 +0900

    Docs: Add a simple chat API

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,13 @@ const register = require("./user/register.js");
 const auth = require("./user/auth.js");
 const login = require("./user/login.js");
 const logout = require("./user/logout.js")
+const chat = require("./chat/chat.js")
 
 
 router.use("/register", register);
 router.use("/auth", auth);
 router.use("/login", login);
 router.use("/logout", logout);
+router.use("/chat", chat);
 
 module.exports = router;
