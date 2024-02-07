f02bb9324ccc6eead2fa4bf49812755a6bd5c255
vesselofgod <vesselofgod@naver.com>
2024-02-07 19:42:21 +0900

    feat: Implement the clinic create route

---
+++ routes/clinic/create.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const Clinic = require("../../model/clinic");
+const router = express.Router();
+
+router.post("/", async (req, res) => {
+  try {
+    const { title, content, doctor_name, specialization, patient_name ,patient_age, patient_gender } =
+      req.body;
+    //const token = req.header("authorization").split(" ")[1];
+    //const user_data = utils.parseJWTPayload(token);
+    console.log(req.body);
+    let clinic = new Clinic({
+      title: title,
+      content: content,
+      "doctor.name": doctor_name,
+      "doctor.specialization": specialization,
+      "patient.name": patient_name,
+      "patient.age": patient_age,
+      "patient.gender": patient_gender,
+      date: Date.now(),
+    });
+
+    await clinic.save(async (err, doc) => {
+      if (err) {
+        console.log(err);
+        return res.status(400).json({
+          success: false,
+          error: [{ msg: "clinic upload failed!" }],
+        });
+      }
+
+      return res.status(200).json({
+        //추가적으로 데이터가 필요하다면 추가할 것.
+        success: true,
+      });
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      success: false,
+      err,
+    });
+  }
+});
+
+module.exports = router;
