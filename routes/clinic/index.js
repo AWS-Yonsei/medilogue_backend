1bb33e3279289de7cd3aa5d98bf763cfaaae133f
vesselofgod <vesselofgod@naver.com>
2024-02-07 19:44:11 +0900

    feat: Add a clinic CRUD APIs

---
+++ routes/clinic/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+const create = require("./create.js");
+const read = require("./read.js");
+const update = require("./update.js");
+
+router.use("/create", create);
+router.use("/read", read);
+router.use("/update", update);
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,15 @@ const router = express.Router();
 const register = require("./user/register.js");
 const auth = require("./user/auth.js");
 const login = require("./user/login.js");
-const logout = require("./user/logout.js")
+//const logout = require("./user/logout.js")
 const chat = require("./chat/chat.js")
-
+const clinic = require("./clinic/index.js")
 
 router.use("/register", register);
 router.use("/auth", auth);
 router.use("/login", login);
-router.use("/logout", logout);
+//router.use("/logout", logout);
 router.use("/chat", chat);
+router.use("/clinic", clinic);
 
 module.exports = router;
