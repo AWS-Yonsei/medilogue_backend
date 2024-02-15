f9209b81c55ae29a79947635bca4ac85327a939b
vesselofgod <vesselofgod@naver.com>
2024-02-16 02:21:33 +0900

    fix: Modify the quiz model to add a reason field

--- model/quiz.js
+++ model/quiz.js
@@ -1,21 +1,24 @@
 const mongoose = require("mongoose");
+const autoIdSetter = require("./auto_increment.js");
 
 const quizSchema = mongoose.Schema({
   qid: {
-    type: String,
-    required: true
+    type: Number
   },
-  content: {
+  question: {
     type: String,
     required: true
   },
   answer: {
-    type: Boolean,
-    required: true
+    type: String,
+    enum: ['O', 'X']
   },
+  reason: {
+    type: String,
+  }
 });
-
+autoIdSetter(quizSchema, mongoose, "quiz", "qid");
 // 데이터베이스 모델을 정의
-const quiz = mongoose.model("message", quizSchema);
+const quiz = mongoose.model("quiz", quizSchema);
 
 module.exports = quiz;
