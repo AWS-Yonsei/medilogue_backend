0a167ba0c8f3b1da72606cde9ebc3f6572f2940b
vesselofgod <vesselofgod@naver.com>
2024-02-16 22:41:33 +0900

    fix: Add a quiz category due to classifying the quiz

--- model/quiz.js
+++ model/quiz.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
-const autoIdSetter = require("./auto_increment.js");
 
 const quizSchema = mongoose.Schema({
-  qid: {
-    type: Number
+  category: {
+    type: String,
+    required: true
   },
   question: {
     type: String,
@@ -17,8 +17,7 @@ const quizSchema = mongoose.Schema({
     type: String,
   }
 });
-autoIdSetter(quizSchema, mongoose, "quiz", "qid");
-// 데이터베이스 모델을 정의
+
 const quiz = mongoose.model("quiz", quizSchema);
 
 module.exports = quiz;
