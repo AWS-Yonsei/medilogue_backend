58f92dc4da663843aa08f3dbdad428cbe97b6ace
saerouni <luxmea1005@yonsei.ac.kr>
2024-02-23 04:11:26 +0900

    feat: Add gpt and api post function

--- stt/README.md
+++ stt/README.md
@@ -1,19 +1,23 @@
 # `stt`
-이 폴더는 S3 + AWS Lambda 환경에서 Event trigger로 Amazon Transcribe 서비스를 이용하는 기능이 구현되어 있습니다. 
-Amazon Transcribe는 음성 파일을 텍스트로 변환합니다.
+이 폴더는 S3 + AWS Lambda 환경에서 Event trigger로 Amazon Transcribe 서비스와 OpenAI GPT-3.5 모델을 이용하는 기능이 구현되어 있습니다.
+Amazon Transcribe는 음성 파일을 텍스트로 변환하고, OpenAI GPT-3.5 모델은 이 텍스트를 처리합니다.
 
 ## `stt_on_s3_manager.py`
 
-이 파일은 AWS S3의 지정된 디렉토리에 업로드된 새로운 음성파일을 처리하는 데 사용됩니다. Amazon Transcribe 서비스를 사용하여 음성 파일을 텍스트로 변환하는 것입니다.
+이 파일은 AWS S3의 지정된 디렉토리에 업로드된 새로운 음성파일을 처리하는 데 사용됩니다. Amazon Transcribe 서비스를 사용하여 음성 파일을 텍스트로 변환하고, 이 텍스트를 OpenAI의 GPT 모델에 보냅니다.
 
 ### `def lambda_handler`
-
 지정된 디렉토리에서 이벤트가 발생하여 이 함수를 호출했을 때 실행됩니다.
 이 함수는 S3 버킷의 이름과 객체의 키를 이벤트에서 가져옵니다.
 그런 다음, 객체의 키에서 파일 이름을 추출하고, 현재 시간을 기반으로 작업 이름을 생성합니다.
 이 작업 이름과 파일 경로를 사용하여 Transcribe 서비스를 시작합니다.
 이 서비스는 음성 파일을 텍스트로 변환하고, 변환된 텍스트를 지정된 S3 버킷에 JSON 형식으로 저장합니다.
+그 후, 이 텍스트를 OpenAI의 GPT 모델에 보내 처리하고, 처리된 결과를 `clinic/stt` API에 POST 요청으로 보냅니다.
 
-### `def string_converter`
+### `dependencies`
+openai, request는 직접 layer를 추가해줘야 합니다.
 
-이 함수는 datetime 객체를 문자열로 변환하는 데 사용됩니다.
+### `role`
+AWS Lambda 함수를 실행하는 데 사용되는 IAM 역할입니다
+이 역할은 Amazon Transcribe 서비스를 사용할 수 있어야 하며, S3 버킷에 대한 읽기 및 쓰기 권한이 있어야 합니다.
+또한 Error가 발생할 경우, CloudWatch Logs에 로그를 기록할 수 있어야 합니다.
--- stt/stt_on_s3_manager.py
+++ stt/stt_on_s3_manager.py
@@ -1,13 +1,20 @@
 from __future__ import print_function
 import boto3
-import datetime
+import logging
 import time
+import json
+import os
+import openai
+import requests
 
 
 def lambda_handler(event, context):
     """Process new document uploaded to S3 using Amazon Transcribe service"""
     BUCKET_NAME = event['Records'][0]['s3']['bucket']['name']
     OBJECT_NAME = event['Records'][0]['s3']['object']['key']
+
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
 
     # Amazon Transcribe 서비스를 사용하여 음성 파일을 텍스트로 변환
     transcribe_client = boto3.client('transcribe')
@@ -21,17 +28,84 @@ def lambda_handler(event, context):
     try:
         response = transcribe_client.start_transcription_job(
             TranscriptionJobName=f'{job_name}',
+            # Specialty='PRIMARYCARE',
+            # Type='CONVERSATION,'
             Media={'MediaFileUri': job_path},
             MediaFormat='mp3',
             LanguageCode='ko-KR',  # 음성 파일의 언어 코드 (한국어: ko-KR)
             OutputBucketName='ysu-team-001-voice-record',
             OutputKey=f'stt-processed/{file_name}.json'
         )
-        print("transcribe successfully")
+        logger.info(f"transcribe successfully: {response}")
     except Exception as e:
-        print(f"Error starting transcription job: {e}")
+        logger.info(f"Error starting transcription job: {e}")
 
+    # S3에서 JSON 파일을 가져옵니다.
+    s3 = boto3.client('s3')
+    data = s3.get_object(Bucket=BUCKET_NAME, Key=f'stt-processed/{file_name}.json')
+    transcript = json.loads(data['Body'].read().decode('utf-8'))
+    logger.info("did")
 
-def string_converter(o):
-    if isinstance(o, datetime.datetime):
-        return o.__str__()
+    # 파일 이름에서 doctor_id와 patient_id를 추출합니다.
+    file_name_without_extension = os.path.splitext(file_name)[0]
+    doctor_id = str(file_name_without_extension.split('-')[0])
+    patient_id = str(file_name_without_extension.split('-')[1])
+    logger.info("%s %s", doctor_id, patient_id)
+
+    # JSON 파일에서 텍스트를 추출합니다.
+    user_message = transcript['results']['transcripts'][0]['transcript']
+
+    # OpenAI API 키를 설정합니다.
+    openai.api_key = os.getenv('OPENAI_SECRET_KEY')
+    # openai.organization = os.getenv('OPENAI_ORGANIZATION')
+
+    try:
+        # 추출한 텍스트를 OpenAI의 GPT 모델에 보냅니다.
+        completion = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                # {
+                #    "role": "system",
+                #    "content": "I am a medical professional who speaks Korean very fluently. When see colloquial writing that has become strange due to breaks or slurred pronunciation, I am good at replacing it with very well-organized writing."
+                # },
+                # {
+                #    "role": "user",
+                #    "content": f"In this case, the patient and doctor had a video consultation, and since they talked for a long time, there were a lot of topics, so I don't remember the details exactly. This is what it is about.{user_message} Could you organize it so it's easier to see? In particular, it would be nice to organize it by dividing it into categories such as symptoms, follow-up prescriptions, etc. Please interpret words with incorrect pronunciation according to the surrounding context."
+                # }
+                # {
+                #    "role": "system",
+                #    "content": "A medical professional who speaks Korean fluently."
+                # },
+                {
+                    "role": "user",
+                    "content": "hi"
+                }
+            ],
+            max_tokens=5,
+            temperature=0.75
+        )
+        ai_response = completion.choices[0].text.strip()
+        logger.info("AI response: %s", ai_response)
+
+        # clinic/stt API로 응답을 보냅니다.
+        api_endpoint = "http://localhost:8080/clinic/stt"
+        headers = {"Content-Type": "application/json"}
+        payload = {
+            "doctor_id": doctor_id,
+            "patient_id": patient_id,
+            "content": [ai_response]  # GPT 모델의 응답을 content에 추가합니다.
+        }
+
+        response = requests.post(api_endpoint, headers=headers, json=payload)
+        logger.info("Response from clinic/stt API: %s", response.text)
+
+        return {
+            'statusCode': response.status_code,
+            'body': response.text
+        }
+    except Exception as e:
+        logger.error('Error from OpenAI API: %s', e)
+        return {
+            'statusCode': 500,
+            'body': 'Unable to get response from OpenAI API.'
+        }
