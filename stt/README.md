f3317013d62dab9599c5670f00377425e1590aa1
saerouni <luxmea1005@yonsei.ac.kr>
2024-02-23 15:23:36 +0900

    fix: Modified gpt prompt implementing document summary functionality And added save path

--- stt/README.md
+++ stt/README.md
@@ -1,7 +1,7 @@
 # `stt`
 이 폴더는 S3 + AWS Lambda 환경에서 Event trigger로 Amazon Transcribe 서비스와 OpenAI GPT-3.5 모델을 이용하는 기능이 구현되어 있습니다.
 Amazon Transcribe는 음성 파일을 텍스트로 변환하고, OpenAI GPT-3.5 모델은 이 텍스트를 처리합니다.
-
+---
 ## `stt_on_s3_manager.py`
 
 이 파일은 AWS S3의 지정된 디렉토리에 업로드된 새로운 음성파일을 처리하는 데 사용됩니다. Amazon Transcribe 서비스를 사용하여 음성 파일을 텍스트로 변환하고, 이 텍스트를 OpenAI의 GPT 모델에 보냅니다.
@@ -13,7 +13,7 @@ Amazon Transcribe는 음성 파일을 텍스트로 변환하고, OpenAI GPT-3.5 
 이 작업 이름과 파일 경로를 사용하여 Transcribe 서비스를 시작합니다.
 이 서비스는 음성 파일을 텍스트로 변환하고, 변환된 텍스트를 지정된 S3 버킷에 JSON 형식으로 저장합니다.
 그 후, 이 텍스트를 OpenAI의 GPT 모델에 보내 처리하고, 처리된 결과를 `clinic/stt` API에 POST 요청으로 보냅니다.
-
+=> json 형태로 ai_response가 doctor_id, patient_id, content의 형식으로 'output/'에 저장됩니다.
 ### `dependencies`
 openai, request는 직접 layer를 추가해줘야 합니다.
 
--- stt/stt_on_s3_manager.py
+++ stt/stt_on_s3_manager.py
@@ -64,45 +64,56 @@ def lambda_handler(event, context):
         completion = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=[
-                # {
-                #    "role": "system",
-                #    "content": "I am a medical professional who speaks Korean very fluently. When see colloquial writing that has become strange due to breaks or slurred pronunciation, I am good at replacing it with very well-organized writing."
-                # },
-                # {
-                #    "role": "user",
-                #    "content": f"In this case, the patient and doctor had a video consultation, and since they talked for a long time, there were a lot of topics, so I don't remember the details exactly. This is what it is about.{user_message} Could you organize it so it's easier to see? In particular, it would be nice to organize it by dividing it into categories such as symptoms, follow-up prescriptions, etc. Please interpret words with incorrect pronunciation according to the surrounding context."
-                # }
-                # {
-                #    "role": "system",
-                #    "content": "A medical professional who speaks Korean fluently."
-                # },
+                {
+                    "role": "system",
+                    "content": "I am a medical professional who speaks Korean very fluently. When see colloquial writing that has become strange due to breaks or slurred pronunciation, I am good at replacing it with very well-organized writing."
+                },
                 {
                     "role": "user",
-                    "content": "hi"
-                }
+                    "content": f"In this case, the patient and doctor had a video consultation, and since they talked for a long time, there were a lot of topics, so I don't remember the details exactly. This is what it is about.{user_message} Could you organize it so it's easier to see? In particular, it would be nice to organize it by dividing it into categories such as symptoms, follow-up prescriptions, etc. Please interpret words with incorrect pronunciation according to the surrounding context."
+                },
+                {"role": "system", "content": "Give me the transcript"},
+                {"role": "user", "content": f"Summarize this {user_message}, as well-organized article."}
             ],
-            max_tokens=5,
+            max_tokens=300,
             temperature=0.75
         )
-        ai_response = completion.choices[0].text.strip()
+        logger.info("send")
+        ai_response = completion['choices'][0]['message']['content']
         logger.info("AI response: %s", ai_response)
 
-        # clinic/stt API로 응답을 보냅니다.
-        api_endpoint = "http://localhost:8080/clinic/stt"
-        headers = {"Content-Type": "application/json"}
+        s3_client = boto3.client('s3')
+        bucket_name = 'ysu-team-001-voice-record'
+        key = f'output/{file_name}.json'
+
         payload = {
             "doctor_id": doctor_id,
             "patient_id": patient_id,
             "content": [ai_response]  # GPT 모델의 응답을 content에 추가합니다.
         }
+        # Convert the payload to JSON
+        payload_json = json.dumps(payload)
 
-        response = requests.post(api_endpoint, headers=headers, json=payload)
-        logger.info("Response from clinic/stt API: %s", response.text)
+        # Save the JSON file to the 'output' folder in the S3 bucket
+        s3_client.put_object(Body=payload_json, Bucket=bucket_name, Key=key)
 
-        return {
-            'statusCode': response.status_code,
-            'body': response.text
-        }
+        logger.info(f"Saved payload to {bucket_name}/{key}")
+        ## clinic/stt API로 응답을 보냅니다.
+        # api_endpoint = "http://localhost:8080/clinic/stt"
+        # headers = {"Content-Type": "application/json"}
+        # payload = {
+        #    "doctor_id": doctor_id,
+        #    "patient_id": patient_id,
+        #    "content": [ai_response]  # GPT 모델의 응답을 content에 추가합니다.
+        # }
+        #
+        # response = requests.post(api_endpoint, headers=headers, json=payload)
+        # logger.info("Response from clinic/stt API: %s", response.text)
+        #
+        # return {
+        #    'statusCode': response.status_code,
+        #    'body': response.text
+        # }
     except Exception as e:
         logger.error('Error from OpenAI API: %s', e)
         return {
